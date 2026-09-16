679ed427e3d3f4a24727ff7c4450fdea2a1e811d
Omar El Farouk IKHLEF <omarelfarouk.ikhlef@hotmail.com>
2026-09-16 11:48:36 +0200

    Ajout de toString a Fraction et test dans Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour, projet SimpleFraction !");
+        Fraction f = new Fraction();
+        System.out.println(f.toString());
+        assert f.toString().equals("Je suis une fraction.");
     }
 }
