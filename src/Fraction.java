54d1c1de23e91201d81ec0c473288b75637d05e8
Omar El Farouk IKHLEF <omarelfarouk.ikhlef@hotmail.com>
2026-09-16 12:24:55 +0200

    Ajout des constructeurs et toString avec numerateur/denominateur

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,6 +1,24 @@
 public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction(int numerator) {
+        this.numerator = numerator;
+        this.denominator = 1;
+    }
+
+    public Fraction() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
     @Override
     public String toString() {
-        return "Je suis une fraction.";
+        return numerator + "/" + denominator;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Fraction f = new Fraction();
-        System.out.println(f.toString());
-        assert f.toString().equals("Je suis une fraction.");
+        Fraction f1 = new Fraction(3, 4);
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction();
+
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+
+        assert f1.toString().equals("3/4");
+        assert f2.toString().equals("5/1");
+        assert f3.toString().equals("0/1");
     }
 }
